4caf4a83a3b321f964eec5eca2042493353fb9da
kohtzerui <kohtr@icloud.com>
2026-01-29 09:23:58 +0800

    Level-1: Echo

--- src/main/java/Nimbus.java
+++ src/main/java/Nimbus.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Nimbus {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
@@ -5,7 +7,21 @@ public class Nimbus {
         System.out.println(" Hello! I'm Nimbus");
         System.out.println(" What can I do for you?");
         System.out.println(line);
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(line);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            System.out.println(line);
+
+            if (input.equals("bye")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+
+            System.out.println(" " + input);
+            System.out.println(line);
+        }
+        scanner.close();
     }
 }
