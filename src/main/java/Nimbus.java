58c71b5bf8775ca6a2c24dad8bae093271a21c02
kohtzerui <kohtr@icloud.com>
2026-02-04 09:18:57 +0800

    Level-0: Rename to Nimbus and Greet

--- src/main/java/Nimbus.java
+++ src/main/java/Nimbus.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Nimbus {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
@@ -7,31 +5,7 @@ public class Nimbus {
         System.out.println(" Hello! I'm Nimbus");
         System.out.println(" What can I do for you?");
         System.out.println(line);
-
-        Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100];
-        int taskCount = 0;
-
-        while (true) {
-            String input = scanner.nextLine();
-            System.out.println(line);
-
-            if (input.equals("bye")) {
-                System.out.println(" Bye. Hope to see you again soon!");
-                System.out.println(line);
-                break;
-            } else if (input.equals("list")) {
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
-                }
-                System.out.println(line);
-            } else {
-                tasks[taskCount] = input;
-                taskCount++;
-                System.out.println(" added: " + input);
-                System.out.println(line);
-            }
-        }
-        scanner.close();
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
