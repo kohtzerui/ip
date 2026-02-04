483df44a311fd7b8607847786719888ff28b7243
kohtzerui <kohtr@icloud.com>
2026-02-04 09:30:53 +0800

    Level-3: Mark as Done and OOP

--- src/main/java/Nimbus.java
+++ src/main/java/Nimbus.java
@@ -7,7 +7,7 @@ public class Nimbus {
         System.out.println(line);
         java.util.Scanner scanner = new java.util.Scanner(System.in);
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         while (true) {
@@ -19,12 +19,25 @@ public class Nimbus {
                 System.out.println(line);
                 break;
             } else if (input.equals("list")) {
+                System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    System.out.println(" " + (i + 1) + "." + tasks[i]);
                 }
                 System.out.println(line);
+            } else if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                tasks[index].markAsDone();
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   " + tasks[index]);
+                System.out.println(line);
+            } else if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                tasks[index].markAsUndone();
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println("   " + tasks[index]);
+                System.out.println(line);
             } else {
-                tasks[taskCount] = input;
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println(" added: " + input);
                 System.out.println(line);
